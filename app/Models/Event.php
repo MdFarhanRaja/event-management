2b88b5aa1d685661fb97bab6e7ac1f77abc3790b
Farhan <farhan@sequifi.com>
2025-04-14 23:08:49 +0530

    laravel Spatie activity log

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,10 +6,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
+
 
 class Event extends Model
 {
     use HasFactory;
+    use LogsActivity;
+
+    public function getActivitylogOptions(): LogOptions
+    {
+
+        return LogOptions::defaults()
+        ->setDescriptionForEvent(fn(string $eventName) => "This model has been {$eventName}")
+        ->logOnly(['name'])
+        ->useLogName('event');
+
+        //return LogOptions::defaults()->logOnly(['name']);
+        // Chain fluent methods for configuration options
+    }
 
     protected $fillable = ['name', 'description', 'start_time', 'end_time', 'user_id'];
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\Activitylog\Models\Activity;
 
 Route::get('/', function () {
+    return Activity::all();
     return view('welcome');
 });
